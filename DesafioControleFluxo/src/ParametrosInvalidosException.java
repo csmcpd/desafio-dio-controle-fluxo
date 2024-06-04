0a0ef2b60f1ef0094fdd4bb5bd26734a10c9dcc1
csmcpd <csmaciel81@gmail.com>
2024-06-04 17:47:05 -0300

    desafio controle de fluxo

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+public class ParametrosInvalidosException extends Exception{
+
+    public void toString(String mensagem) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(mensagem);
+    }
+    
+}
